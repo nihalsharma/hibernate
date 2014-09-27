448aa826db23fd5a0230f9743a3751625b84eaf7
nihalsharma <nihaldps@gmail.com>
2014-09-27 20:22:06 +0530

    Update DBServiceInaccessible.java

--- midservice-client/src/main/java/com/tm/mid/client/exception/DBServiceInaccessible.java
+++ midservice-client/src/main/java/com/tm/mid/client/exception/DBServiceInaccessible.java
@@ -3,13 +3,13 @@ package com.tm.mid.client.exception;
 /**
  * Created by BigP on 9/17/14.
  */
-public class TMMIDServiceInaccessible extends Exception {
+public class DBServiceInaccessible extends Exception {
 
-    public TMMIDServiceInaccessible(String message, Throwable cause) {
+    public DBServiceInaccessible(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public TMMIDServiceInaccessible(String message) {
+    public DBServiceInaccessible(String message) {
         super(message);
     }
 
